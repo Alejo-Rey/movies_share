49695f722eb3859e98eaf5c1492fde0f6adf752b
Alejo-Rey <771@holbertonschool.com>
2020-03-29 19:37:16 +0000

    fix pass

--- database/script.rb
+++ database/script.rb
@@ -1,6 +1,6 @@
 require 'sequel'
 
-DB = Sequel.connect("postgres://#{ENV['USER_DB']}@#{ENV['HOST_DB']}:#{ENV['PORT_DB']}/#{ENV['NAME_DB']}")
+DB = Sequel.connect("postgres://#{ENV['USER_DB']}:#{ENV['USER_PASS_DB']}@#{ENV['HOST_DB']}:#{ENV['PORT_DB']}/#{ENV['NAME_DB']}")
 
 DB.create_table :movies do
     primary_key :id
